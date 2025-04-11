122cda33d40925a3ddfe7c663a27e70cefba0101
Jacob Reger <regerjacob@gmail.com>
2025-04-10 18:04:24 -0700

    just use the current window

--- src/nvim.rs
+++ src/nvim.rs
@@ -1,8 +1,9 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, usize};
 
 use futures::StreamExt;
 use log::{debug, error};
-use nvim_rs::{compat::tokio::Compat, Buffer, Neovim, Value};
+use nvim_rs::{compat::tokio::Compat, Buffer, Neovim, Value, Window};
+use ratatui::crossterm::event::{self, Event, KeyCode};
 use tokio::{io::WriteHalf, net::UnixStream};
 
 use crate::{
@@ -29,65 +30,13 @@ pub async fn select_callback(socket: PathBuf, selection: Ref) -> Result<()> {
             panic!()
         });
 
-    // Hide the current window
-    //nvim.get_current_win().await?.hide().await?;
+    let self_win = nvim.get_current_win().await?;
+    self_win.hide().await?;
 
     // Select the proper window
     let wins = nvim.list_wins().await?;
 
-    //for win in &wins {
-    //    let buf = nvim.create_buf(false, true).await?;
-    //    buf.set_lines(0, 0, false, vec!["Hello world :)".into()])
-    //        .await?;
-    //    let width = 20;
-    //    let height = 3;
-    //    let center_x = win.get_width().await? / 2;
-    //    let center_y = win.get_height().await? / 2;
-    //
-    //    let mut config = Vec::new();
-    //    config.push((
-    //        Value::String("relative".into()),
-    //        Value::String("win".into()),
-    //    ));
-    //    config.push((Value::String("win".into()), win.get_value().clone()));
-    //    config.push((Value::String("width".into()), Value::Integer(width.into())));
-    //    config.push((
-    //        Value::String("height".into()),
-    //        Value::Integer(height.into()),
-    //    ));
-    //    config.push((
-    //        Value::String("col".into()),
-    //        Value::Integer((center_x - width / 2).into()),
-    //    ));
-    //    config.push((
-    //        Value::String("row".into()),
-    //        Value::Integer((center_y - height / 2).into()),
-    //    ));
-    //    config.push((Value::String("focusable".into()), Value::Boolean(false)));
-    //    config.push((
-    //        Value::String("style".into()),
-    //        Value::String("minimal".into()),
-    //    ));
-    //    let border = vec![
-    //        Value::String("╭".into()),
-    //        Value::String("─".into()),
-    //        Value::String("╮".into()),
-    //        Value::String("│".into()),
-    //        Value::String("╯".into()),
-    //        Value::String("─".into()),
-    //        Value::String("╰".into()),
-    //        Value::String("│".into()),
-    //    ];
-    //    config.push((Value::String("border".into()), Value::Array(border)));
-    //
-    //    nvim.open_win(&buf, false, config).await?;
-    //}
-
-    // Check if the correct file is already open in a buffer and switch to it
-    //let buf = find_or_open_buf(&nvim, &selection).await?;
-    //wins[0].set_buf(&buf).await?;
-    //wins[0].set_cursor((selection.line as i64, selection.column as i64)).await?;
-
+    //select_window(&nvim).await?;
 
     for buf in nvim.list_bufs().await? {
         debug!(
@@ -99,12 +48,13 @@ pub async fn select_callback(socket: PathBuf, selection: Ref) -> Result<()> {
             debug!("Found match!");
             debug!("Buflisted: {}", buf.get_option("buflisted").await?);
             buf.set_option("buflisted", Value::Boolean(true)).await?;
-            wins[0].set_buf(&buf).await?;
+            nvim.get_current_win().await?.set_buf(&buf).await?;
+            //wins[0].set_buf(&buf).await?;
             break;
         }
     }
 
-    if let Err(err) = wins[0]
+    if let Err(err) = nvim.get_current_win().await?
         .set_cursor((selection.line as i64, selection.column as i64))
         .await
     {
@@ -129,4 +79,129 @@ async fn find_or_open_buf(nvim: &Neovim<Compat<WriteHalf<UnixStream>>>, selectio
     nvim.command(format!("edit {}", selection.file.display()).as_str()).await?;
     nvim.set_current_buf(&prev_buf).await?;
     Ok(buf)
+}
+
+async fn select_window(nvim: &Neovim<Compat<WriteHalf<UnixStream>>>) -> Result<Window<Compat<WriteHalf<UnixStream>>>> {
+    // Hide the current window
+    let app_win = nvim.get_current_win().await?;
+    debug!("app_win is: {}", app_win.get_buf().await?.get_name().await?);
+
+    let mut config = Vec::new();
+    config.push((
+        Value::String("relative".into()),
+        Value::String("editor".into()),
+    ));
+    config.push((Value::String("width".into()), Value::Integer(1.into())));
+    config.push((
+        Value::String("height".into()),
+        Value::Integer(1.into()),
+    ));
+    config.push((
+        Value::String("col".into()),
+        Value::Integer(0.into()),
+    ));
+    config.push((
+        Value::String("row".into()),
+        Value::Integer(0.into()),
+    ));
+    config.push((Value::String("focusable".into()), Value::Boolean(true)));
+    config.push((
+        Value::String("style".into()),
+        Value::String("minimal".into()),
+    ));
+
+    let invis_win = nvim.open_win(&app_win.get_buf().await?, true, config).await?;
+    debug!("Opened invisible window to capture input");
+
+    app_win.hide().await?;
+    debug!("Hidden previous app window");
+
+    let wins = nvim.list_wins().await?;
+
+    struct SelectableWindow {
+        window: Window<Compat<WriteHalf<UnixStream>>>,
+        select_prompt: Window<Compat<WriteHalf<UnixStream>>>,
+    }
+
+    let mut selectable_wins: Vec<SelectableWindow> = Vec::new();
+
+    for (i, win) in wins.iter().enumerate() {
+        // Empty indicates a normal buffer. If it is any other kind of buffer, we will assume that
+        // we cannot select this window for opening text files.
+        if win.get_buf().await?.get_option("buftype").await? != Value::String("".into()) {
+            continue;
+        }
+
+        // Create our buffer and gather some sizing information
+        let buf = nvim.create_buf(false, true).await?;
+        buf.set_lines(0, 0, false, vec![format!("   Pick Window {i}   ").into()])
+            .await?;
+        let width = 19;
+        let height = 1;
+        let center_x = win.get_width().await? / 2;
+        let center_y = win.get_height().await? / 2;
+
+        // Configure our buffer
+        let mut config = Vec::new();
+        config.push((
+            Value::String("relative".into()),
+            Value::String("win".into()),
+        ));
+        config.push((Value::String("win".into()), win.get_value().clone()));
+        config.push((Value::String("width".into()), Value::Integer(width.into())));
+        config.push((
+            Value::String("height".into()),
+            Value::Integer(height.into()),
+        ));
+        config.push((
+            Value::String("col".into()),
+            Value::Integer((center_x - width / 2).into()),
+        ));
+        config.push((
+            Value::String("row".into()),
+            Value::Integer((center_y - height / 2).into()),
+        ));
+        config.push((Value::String("focusable".into()), Value::Boolean(false)));
+        config.push((
+            Value::String("style".into()),
+            Value::String("minimal".into()),
+        ));
+        let border = vec![
+            Value::String("╭".into()),
+            Value::String("─".into()),
+            Value::String("╮".into()),
+            Value::String("│".into()),
+            Value::String("╯".into()),
+            Value::String("─".into()),
+            Value::String("╰".into()),
+            Value::String("│".into()),
+        ];
+        config.push((Value::String("border".into()), Value::Array(border)));
+
+        // Open a new floating window in the center of the existing text editing window with our
+        // hello window
+        let prompt = nvim.open_win(&buf, false, config).await?;
+        selectable_wins.push(SelectableWindow { window: win.clone(), select_prompt: prompt });
+    }
+
+    // Loop to gather input
+    loop {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                continue;
+            }
+            match key.code {
+                KeyCode::Char(ch) => {
+                    if let Some(num) = ch.to_digit(10) {
+                        for selectable_win in &selectable_wins {
+                            selectable_win.select_prompt.close(false).await?;
+                        }
+
+                        return Ok(selectable_wins[num as usize].window.clone());
+                    }
+                }
+                _ => continue,
+            }
+        }
+    }
 }
